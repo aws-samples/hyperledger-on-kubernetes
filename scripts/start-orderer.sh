1631cbfbd2a201c02129c9cce24e5e2a9f170f07
Michael Edge <edgema@amazon.com>
2018-12-19 10:48:56 +0800

    updated to Fabric v1.4

--- scripts/start-orderer.sh
+++ scripts/start-orderer.sh
@@ -12,8 +12,28 @@ source $(dirname "$0")/env.sh
 
 log "Preparing to start orderer host '$ORDERER_HOST:$ORDERER_PORT', enrolled via '$ENROLLMENT_URL' with MSP at '$ORDERER_GENERAL_LOCALMSPDIR'"
 
-# Wait for setup to complete sucessfully
-#awaitSetup
+# Install fabric-ca. Recent version of Hyperledger Fabric do not include a fabric-ca-tools, fabric-ca-peer, etc., where the
+# fabric-ca-client is included. So we will need to build fabric-ca-client ourselves.
+wget https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
+tar -xzf go1.10.3.linux-amd64.tar.gz
+mv go /usr/local
+sleep 5
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOROOT/bin:$PATH
+export PATH=$PATH:$HOME/go/src/github.com/hyperledger/fabric-ca/bin
+apt-get update
+apt-get install git-core -y
+apt-get install libtool libltdl-dev -y
+apt-get install build-essential -y
+sleep 5
+go get -u github.com/hyperledger/fabric-ca/cmd/...
+sleep 10
+cd $HOME/go/src/github.com/hyperledger/fabric-ca
+make fabric-ca-client
+sleep 5
+export PATH=$PATH:$HOME/go/src/github.com/hyperledger/fabric-ca/bin
+
 
 # Enroll to get orderer's TLS cert (using the "tls" profile)
 fabric-ca-client enroll -d --enrollment.profile tls -u $ENROLLMENT_URL -M /tmp/tls --csr.hosts $ORDERER_HOST
@@ -31,9 +51,6 @@ fabric-ca-client enroll -d -u $ENROLLMENT_URL -M $ORDERER_GENERAL_LOCALMSPDIR
 # Finish setting up the local MSP for the orderer
 finishMSPSetup $ORDERER_GENERAL_LOCALMSPDIR
 copyAdminCert $ORDERER_GENERAL_LOCALMSPDIR
-
-# Wait for the genesis block to be created
-#dowait "genesis block to be created" 60 $SETUP_LOGFILE $ORDERER_GENERAL_GENESISFILE
 
 # Start the orderer
 log "Starting orderer host '$ORDERER_HOST' with MSP at '$ORDERER_GENERAL_LOCALMSPDIR'"
