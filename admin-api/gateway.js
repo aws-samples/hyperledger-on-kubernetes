5c06711ac59fe925724e62dd0a33fe8af9650af6
Michael Edge <edgema@amazon.com>
2019-02-01 14:40:04 +0800

    add admin api

--- admin-api/gateway.js
+++ admin-api/gateway.js
@@ -1077,4 +1077,4 @@ exports.getPortsFromEnv = getPortsFromEnv;
 exports.installChaincode = installChaincode;
 exports.instantiateChaincode = instantiateChaincode;
 exports.uploadMSPtoS3 = uploadMSPtoS3;
-exports.downloadMSPtoS3 = downloadMSPtoS3;
+exports.downloadMSPfromS3 = downloadMSPfromS3;
